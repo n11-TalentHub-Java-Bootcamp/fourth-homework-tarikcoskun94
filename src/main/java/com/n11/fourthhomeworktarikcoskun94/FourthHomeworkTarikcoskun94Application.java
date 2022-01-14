834f209939ef552a13c14c7ca2d32f443391c7bc
Tarık COŞKUN <tarikcoskun@hotmail.com.tr>
2022-01-14 20:29:31 +0300

    Initial Spring Boot project was created.

---
+++ src/main/java/com/n11/fourthhomeworktarikcoskun94/FourthHomeworkTarikcoskun94Application.java
@@ -0,0 +1,14 @@
+package com.n11.fourthhomeworktarikcoskun94;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FourthHomeworkTarikcoskun94Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FourthHomeworkTarikcoskun94Application.class, args);
+        System.out.println("Hello Spring Boot");
+    }
+
+}
---
+++ src/test/java/com/n11/fourthhomeworktarikcoskun94/FourthHomeworkTarikcoskun94ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.n11.fourthhomeworktarikcoskun94;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FourthHomeworkTarikcoskun94ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
